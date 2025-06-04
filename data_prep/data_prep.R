05e66324fbbbfb958fe8e4bf9132f28187a62b77
andymccannnisra <andrew.mcccann@nisra.gov.uk>
2025-06-04 12:56:45 +0100

    UPdate for chart1 data

--- data_prep/data_prep.R
+++ data_prep/data_prep.R
@@ -34,18 +34,57 @@ ylt_currentyr_plus1_data <- read.spss(
 
 ################################################################################
 
-# Step 1: Build the initial response counts
+# Step 1: Build the initial response counts (exclude NA entirely)
+chart1_previousyr_data <- nilt_previousyr_data %>%
+  select(GBVPHYV, GBVSEXV, GBVPSYV, GBVECONV, GBVONLV) %>%
+  pivot_longer(everything(), names_to = "Variable", values_to = "Response") %>%
+  filter(!is.na(Response)) %>%      # Exclude NA values entirely at the start
+  mutate(Response = as.character(Response)) %>%
+  count(Response, Variable, name = "Count") %>%
+  complete(Response = c("Yes", "No"), Variable, fill = list(Count = 0)) %>%
+  pivot_wider(names_from = Variable, values_from = Count) %>%
+  rename(GBVPHYVA = GBVPHYV,
+         GBVSEXVA = GBVSEXV,
+         GBVPSYVA = GBVPSYV) %>%
+  arrange(factor(Response, levels = c("Yes", "No")))
+
+# Step 2: Add total row (only Yes + No included)
+total_row <- chart1_previousyr_data %>%
+  select(-Response) %>%
+  summarise(across(everything(), sum)) %>%
+  mutate(Response = "Total") %>%
+  select(Response, everything())
+
+chart1_previousyr_data <- bind_rows(chart1_previousyr_data, total_row)
+
+# Step 3: Calculate experienced_violence_perc (no rounding, no NA)
+experienced_violence_perc <- chart1_previousyr_data %>%
+  filter(Response %in% c("Yes", "No")) %>%
+  pivot_longer(-Response, names_to = "Variable", values_to = "Count") %>%
+  pivot_wider(names_from = Response, values_from = Count, values_fill = 0) %>%
+  mutate(TotalYN = Yes + No,
+         Percent = (Yes / TotalYN) * 100) %>%
+  select(Variable, Percent) %>%
+  pivot_wider(names_from = Variable, values_from = Percent) %>%
+  mutate(Response = "experienced_violence_perc") %>%
+  select(Response, everything())
+
+# Step 4: Bind it all together
+chart1_previousyr_data <- bind_rows(chart1_previousyr_data, experienced_violence_perc)
+
+
+# Step 1: Build the initial response counts (exclude NA entirely)
 chart1_currentyr_data <- nilt_currentyr_data %>%
   select(GBVPHYVA, GBVSEXVA, GBVPSYVA, GBVECONV, GBVONLV) %>%
   pivot_longer(everything(), names_to = "Variable", values_to = "Response") %>%
-  mutate(Response = as.character(Response),
-         Response = ifelse(is.na(Response), "NA", Response)) %>%
+  filter(!is.na(Response)) %>%      # Exclude NA values entirely at the start
+  mutate(Response = as.character(Response)) %>%
   count(Response, Variable, name = "Count") %>%
-  complete(Response = c("Yes", "No", "NA"), Variable, fill = list(Count = 0)) %>%
+  complete(Response = c("Yes", "No"), Variable, fill = list(Count = 0)) %>%
   pivot_wider(names_from = Variable, values_from = Count) %>%
-  arrange(factor(Response, levels = c("Yes", "No", "NA")))
+  arrange(factor(Response, levels = c("Yes", "No")))
 
-# Step 2: Add total row
+# Step 2: Add total row (only Yes + No included)
 total_row <- chart1_currentyr_data %>%
   select(-Response) %>%
   summarise(across(everything(), sum)) %>%
@@ -54,12 +93,13 @@ total_row <- chart1_currentyr_data %>%
 
 chart1_currentyr_data <- bind_rows(chart1_currentyr_data, total_row)
 
-# Step 3: Add experienced_violence_perc row
+# Step 3: Calculate experienced_violence_perc (no rounding, no NA)
 experienced_violence_perc <- chart1_currentyr_data %>%
-  filter(Response %in% c("Yes", "Total")) %>%
+  filter(Response %in% c("Yes", "No")) %>%
   pivot_longer(-Response, names_to = "Variable", values_to = "Count") %>%
-  pivot_wider(names_from = Response, values_from = Count) %>%
-  mutate(Percent = (Yes / Total) * 100) %>%
+  pivot_wider(names_from = Response, values_from = Count, values_fill = 0) %>%
+  mutate(TotalYN = Yes + No,
+         Percent = (Yes / TotalYN) * 100) %>%
   select(Variable, Percent) %>%
   pivot_wider(names_from = Variable, values_from = Percent) %>%
   mutate(Response = "experienced_violence_perc") %>%
