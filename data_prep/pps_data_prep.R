7f706f9b1fb6ee85bf1226080588696bc96a3b40
danielnisra <firstname.lastname@nisra.gov.uk>
2025-08-08 11:39:24 +0100

    changing source to data portal

--- data_prep/pps_data_prep.R
+++ data_prep/pps_data_prep.R
@@ -6,40 +6,49 @@ source(paste0(here(), "/config.R"))
 # READ IN THE DATA
 
 # URL being functioned and saved onto a temp file
-pps_url <-"https://www.ppsni.gov.uk/files/ppsni/2024-06/PPS%20Statistical%20Bulletin%202023_24%20Tables.xlsx"
-temp_file <- tempfile(fileext = ".xlsx")
+# pps_url <-"https://www.ppsni.gov.uk/files/ppsni/2024-06/PPS%20Statistical%20Bulletin%202023_24%20Tables.xlsx"
+# temp_file <- tempfile(fileext = ".xlsx")
+# 
+# # Download the file using httr::GET
+# GET(pps_url,
+#     write_disk(temp_file,
+#                overwrite = TRUE),
+#     httr::config(ssl_verifypeer = FALSE))
+# 
+# # Create a data frame for the data in the temp file
+# files_received_pps <- read_excel(temp_file,
+#                                      sheet = "Table 1B",
+#                                      range = "A3:D15")
+# 
+# victims_pps <- read_excel(temp_file,
+#                                     sheet = "Table 6B",
+#                                     range = "A3:F13")
+# 
+# # Define URL and temp file
+# pps_url <- "https://www.justice-ni.gov.uk/sites/default/files/publications/justice/202324%20case%20processing%20time%20tables%20-%20%20open%20data%20format.ods"
+# temp_file <- tempfile(fileext = ".ods")
+# 
+# # Download the file
+# GET(pps_url, write_disk(temp_file, overwrite = TRUE))
+# 
+# # Now read the .ods file from disk
+# time_taken_pps <- read_ods(temp_file, sheet = "5", range = "A4:G17")
 
-# Download the file using httr::GET
-GET(pps_url,
-    write_disk(temp_file,
-               overwrite = TRUE),
-    httr::config(ssl_verifypeer = FALSE))
+pps_data_portal <- read.csv("https://ws-data.nisra.gov.uk/public/api.restful/PxStat.Data.Cube_API.ReadDataset/INDPRCASEEQ/CSV/1.0/en")
 
-# Create a data frame for the data in the temp file
-files_received_pps <- read_excel(temp_file,
-                                     sheet = "Table 1B",
-                                     range = "A3:D15")
-
-victims_pps <- read_excel(temp_file,
-                                    sheet = "Table 6B",
-                                    range = "A3:F13")
-
-
-# Define URL and temp file
-pps_url <- "https://www.justice-ni.gov.uk/sites/default/files/publications/justice/202324%20case%20processing%20time%20tables%20-%20%20open%20data%20format.ods"
-temp_file <- tempfile(fileext = ".ods")
-
-# Download the file
-GET(pps_url, write_disk(temp_file, overwrite = TRUE))
-
-# Now read the .ods file from disk
-time_taken_pps <- read_ods(temp_file, sheet = "5", range = "A4:G17")
+time_taken_pps <- pps_data_portal %>% 
+  filter(Financial.year %in% c("2019/20", "2020/21", "2021/22", "2022/23", "2023/24") & grepl("Offence category", Equality.Groups)) %>% 
+  mutate(Equality.Groups = str_sub(Equality.Groups, unlist(gregexpr("-", Equality.Groups)) + 2, nchar(Equality.Groups))) %>% 
+  pivot_wider(id_cols = Equality.Groups,
+              names_from = Financial.year,
+              values_from = VALUE,
+              names_glue = "Median Days taken in {Financial.year}")
 
 ################################################################################
 ### CHART 3 DATA PREP FOR PPS (Median time taken for cases to be dealt with at courts by offence type) 
 
 # Select and rename columns
-pps_chart3_data <- time_taken_pps[, c(1, ncol(time_taken_pps) - 1)]
+pps_chart3_data <- time_taken_pps[, c(1, ncol(time_taken_pps))]
 colnames(pps_chart3_data) <- c("offence", "days")
 
 # Sort the data frame by descending 'days'
