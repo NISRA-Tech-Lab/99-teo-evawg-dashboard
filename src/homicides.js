d0894480c8cafcb87ec0939336a28d008c4516e4
andymccannnisra <andrew.mcccann@nisra.gov.uk>
2025-12-02 11:33:56 +0000

    homicide update

--- src/homicides.js
+++ src/homicides.js
@@ -3,6 +3,7 @@ import { maleComparison } from "./utils/male-comparison.js";
 import { readData } from "./utils/read-data.js";
 import { createMaleFemaleLineChart } from "./utils/charts.js";
 import { years, latest_year, updateYearSpans } from "./utils/update-years.js";
+import { insertValue } from "./utils/insert-value.js";
 
 window.addEventListener("DOMContentLoaded", async () => {
 
@@ -17,8 +18,24 @@ window.addEventListener("DOMContentLoaded", async () => {
     const stat = "All homicides";
 
     updateYearSpans(data, stat);
+
+    insertValue("homicide-box1-female", data.data[stat][latest_year]["18+ years"]["Female"]);
+    insertValue("homicide-box1-male", data.data[stat][latest_year]["18+ years"]["Male"]);
+    insertValue("homicide-box2-female", data.data[stat][latest_year]["18+ years"]["Female"]);
+    insertValue("homicide-box2-male", data.data[stat][latest_year]["18+ years"]["Male"]);
+    insertValue("homicide-box3-girl", data.data[stat][latest_year]["Under 18 years"]["Female"]);
+    insertValue("homicide-box3-boy", data.data[stat][latest_year]["Under 18 years"]["Male"]);
     
     // Create line chart
+        createMaleFemaleLineChart({
+        data,
+        stat,
+        years,
+        female_selection: ["Under 18 years", "Female"],
+        male_selection: ["Under 18 years", "Male"],
+        canvas_id: "under-18-homicide-line"
+    });
+
     createMaleFemaleLineChart({
         data,
         stat,
